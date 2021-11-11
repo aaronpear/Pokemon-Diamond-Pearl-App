ba715ee1bd5889f5fb8e06cc44ff97e08424656a
Aaron Reyes <56135909+aaronpear@users.noreply.github.com>
2021-11-11 14:22:59 -0800

    Base react app created, styles cleared

---
+++ pokemon-diamond-pearl-app/src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import axios from 'axios';
+import { useState, useEffect } from 'react'
+
+function App() {
+  // const [allPokemon, setAllPokemon] = useState([]);
+
+  // useEffect(() => {
+  //   axios.get()
+  //     .then(response => {
+
+  //     })
+  //     .catch(error => {
+
+  //     })
+  // })
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Pick Your Team!</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
